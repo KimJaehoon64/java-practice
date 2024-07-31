054f55a35e9f6cfc011a6b2ce60fdf333031f014
KimJaehoon64 <0192985468@naver.com>
2024-07-31 16:26:22 +0900

    level01 문제 풀이

---
+++ level01/basic/Application2.java
@@ -0,0 +1,12 @@
+package com.jaehoon.level01.basic;
+
+public class Application2 {
+    public static void main(String[] args) {
+        double width = 12.5;
+        double height = 36.4;
+
+        System.out.println("면적 : " + (width * height));
+        System.out.println("둘레 : " + ((width * 2) + (height * 2)));
+
+    }
+}
