ec22e95d7a43a08f1d64af6bac36aa725bc0ce9c
KimJaehoon64 <0192985468@naver.com>
2024-08-01 23:38:48 +0900

    fix : Application3 코드 재 작성

--- chap04/section02/level01/basic/Application3.java
+++ chap04/section02/level01/basic/Application3.java
@@ -1,16 +1,21 @@
 package com.jaehoon.chap04.section02.level01.basic;
 
 import java.util.Scanner;
-
+/* 1부터 입력 받은 정수까지의 합 */
 public class Application3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("문자열을 입력하세요: ");
-        String str = in.nextLine(); // 입력받은 문자열 대입
-        char ch = str.charAt(0);
-        for(int i = 0; i < str.length(); i++) {
-            ch = str.charAt(i);
-            System.out.println(i + ":" + ch );
+        System.out.println("정수를 입력하세요 : ");
+        int n = in.nextInt();
+        int sum = 0;
+
+
+        for(int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
         }
-    }
-}
+        System.out.println("1부터" + n + "까지 짝수의 합: " + sum);
+      }
+   }
+
