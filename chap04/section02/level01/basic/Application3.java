e0b91579e9966931263ae3816287bd0cbef5f2aa
KimJaehoon64 <0192985468@naver.com>
2024-08-01 23:40:14 +0900

    comment 주석 수정

--- chap04/section02/level01/basic/Application3.java
+++ chap04/section02/level01/basic/Application3.java
@@ -1,7 +1,7 @@
 package com.jaehoon.chap04.section02.level01.basic;
 
 import java.util.Scanner;
-/* 1부터 입력 받은 정수까지의 합 */
+/* 1부터 입력 받은 정수까지의 짝수의 합 */
 public class Application3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
